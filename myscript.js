b03f5fd5cc727a467a02dedcd7be66b5acf66b85
Serpej <Jehag@ive.se>
2024-11-14 12:44:17 +0100

    Add container and loop that creates and appends squares inside the container

--- myscript.js
+++ myscript.js
@@ -1,12 +1,27 @@
+const body = document.querySelector(".body");
+
 const edge = document.querySelector("#edge");
 
 const reset = document.createElement("button");
 reset.classList.add("resetB");
 reset.textContent = "Reset"
-edge.appendChild(reset);
+body.insertBefore(reset, edge);
 //eventlistener with  a window prompt (type number). The answer goes into a a conditional that makes sure the number is under 100 and then creates that many squares in the edge. 
 
 
 const gridSquare = document.createElement("div");
 gridSquare.classList.add("square");
-//eventlistener on load to create 256 squares in a flexbox grid.
+
+
+let i = 0;
+let numberOfSquares = 255;
+while (i <= numberOfSquares) {
+  
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("square");
+    edge.appendChild(gridSquare);
+    console.log(i);
+    i++;
+};
+
+/* eventlistener on load to create 256 squares in a flexbox grid. */
