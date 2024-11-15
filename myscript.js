611288ccc4bfd5daa3f176c2497fcf244f2ef1aa
Serpej <Jehag@ive.se>
2024-11-15 12:15:59 +0100

    Add while(true) loop to keep asking for right value when the reset button is pressed.

--- myscript.js
+++ myscript.js
@@ -32,18 +32,29 @@ window.addEventListener("load", () => {let i = 0;
 });
 
 reset.addEventListener("click", () =>{
-    let newSquares = Number(window.prompt("How many number of squares per side do you want in your grid?", "50"));
-        
-        if(newSquares>100 || newSquares<0) {
-            let newSquares = Number(window.prompt("Invalid input, choose between 0 and 100 per side", "50"));
-            console.log(newSquares);
-            //use continue here?
+    let newSquares;
 
-        } else if (newSquares == NaN) {
-            let newSquares = Number(window.prompt("Invalid input, choose between 0 and 100 per side", "50"));
-            console.log(newSquares);
-            //use continue here?}
+    while(true) {
+        let input = window.prompt("How many number of squares per side do you want in your grid?", "50");
+
+        if (input === null) {
+            return;
+        }
         
-        } else {console.log(newSquares); return newSquares};
+        newSquares = Number(input);
+
+        if (isNaN(newSquares)) {
+            alert("Invalid input, choose between 0 and 100 per side", "50");
+            console.log(newSquares);
+
+        } else if(newSquares>100 || newSquares<0) {
+            alert("Invalid input, choose between 0 and 100 per side", "50");
+            console.log(newSquares);
+
+        } else {
+            console.log(`User selected ${newSquares} squares per side.`);
+            return newSquares;
+        };
+    };
 });
 
